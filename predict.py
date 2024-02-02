124d1fd8534627ce3225b808fe25e91b021cd626
bae-hong-seob <bhs8931@gmail.com>
2024-02-02 13:08:48 +0000

    add: complete inference

--- predict.py
+++ predict.py
@@ -66,58 +66,36 @@ def main():
     print(f'--------------- {config['model']} MODEL LOAD---------------')
     dims = [ae_dataloader.num_item] + config['dims'] #num_item = 6,807
     model = models_load(config, dims)
-    print(model)
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=config['learning_rate'])
+    model.load_state_dict(torch.load(os.path.join(config['model_path'], config['model']+'.pt')))
     
-    ######################## TRAIN
-    print(f'--------------- {config['model']} TRAINING ---------------')
-    best_hit = 0
-    save_time = str(setting.save_time)
-    for epoch in range(1, config['epochs'] + 1):
-        tbar = tqdm(range(1))
-        for _ in tbar:
-            train_loss = train(model = model, criterion = criterion, optimizer = optimizer, data_loader = data_loader, make_matrix_data_set = ae_dataloader)
-            ndcg, hit = evaluate(model = model, data_loader = data_loader, user_train = train_dict, user_valid = valid_dict, make_matrix_data_set = ae_dataloader)
+    ndcg, best_hit = evaluate(model = model, data_loader = data_loader, user_train = train_dict, user_valid = valid_dict, make_matrix_data_set = ae_dataloader)
 
-            if best_hit < hit:
-                best_hit = hit
-                torch.save(model.state_dict(), os.path.join(config['model_path'], save_time + '_' + config['model'] + '.pt'))
-
-            tbar.set_description(f'Epoch: {epoch:3d}| Train loss: {train_loss:.5f}| NDCG@10: {ndcg:.5f}| HIT@10: {hit:.5f}')
-    
     ######################## INFERENCE
     print(f'--------------- {config['model']} PREDICT ---------------')
     submission_data_loader = DataLoader(AE_dataset, batch_size = config['batch_size'], shuffle = False, pin_memory = True, num_workers = config['num_workers'])
-    model.load_state_dict(torch.load(os.path.join(config['model_path'], save_time + '_' + config['model'] + '.pt')))
+    model.load_state_dict(torch.load(os.path.join(config['model_path'], config['model']+'.pt')))
     
     user2rec_list = predict(model = model, data_loader = submission_data_loader,user_train = train_dict, user_valid = valid_dict, make_matrix_data_set = ae_dataloader)
     
     
     ######################## SAVE PREDICT
     print(f'--------------- SAVE PREDICT ---------------')
-    with open('record.txt', 'a') as f:
-        f.write(f"Tiemstamp:{setting.save_time}, Recall@10:{best_hit}\n")
-    f.close()
-    
-    predicts = []
+    submission = []
     users = [i for i in range(0, ae_dataloader.num_user)]
     for user in users:
         rec_item_list = user2rec_list[user]
         for item in rec_item_list:
-            predicts.append(
+            submission.append(
                 {   
                     'user' : ae_dataloader.user_decoder[user],
                     'item' : ae_dataloader.item_decoder[item],
                 }
             )
-            
     filename = setting.get_submit_filename(config, best_hit)
     
-    submission = pd.DataFrame(predicts)
+    submission = pd.DataFrame(submission)
     submission.to_csv(filename, index=False)
     print('make csv file !!! ', filename)
-
-
+    
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -26,34 +26,34 @@ class Setting:
         save_time = now_date + '_' + now_hour.replace(':', '')
         self.save_time = save_time
 
-    def get_log_path(self, args):
+    def get_log_path(self, config):
         '''
         [description]
         log file을 저장할 경로를 반환하는 함수입니다.
 
         [arguments]
-        args : argparse로 입력받은 args 값으로 이를 통해 모델의 정보를 전달받습니다.
+        config : argparse로 입력받은 config 값으로 이를 통해 모델의 정보를 전달받습니다.
 
         [return]
         path : log file을 저장할 경로를 반환합니다.
         이 때, 경로는 log/날짜_시간_모델명/ 입니다.
         '''
-        path = f'./log/{self.save_time}_{args.model}/'
+        path = f'./log/{self.save_time}_{config.model}/'
         return path
 
-    def get_submit_filename(self, args, valid_score):
+    def get_submit_filename(self, config, valid_score):
         '''
         [description]
         submit file을 저장할 경로를 반환하는 함수입니다.
 
         [arguments]
-        args : argparse로 입력받은 args 값으로 이를 통해 모델의 정보를 전달받습니다.
+        config : argparse로 입력받은 config 값으로 이를 통해 모델의 정보를 전달받습니다.
 
         [return]
         filename : submit file을 저장할 경로를 반환합니다.
         이 때, 파일명은 submit/날짜_시간_모델명.csv 입니다.
         '''
-        filename = f'./submit/{self.save_time}_{args.model}_{valid_score:.4f}.csv'
+        filename = f'./submit/{self.save_time}_{config['model']}_{valid_score:.4f}.csv'
         return filename
 
     def make_dir(self,path):
@@ -77,10 +77,10 @@ class Setting:
 def models_load(config, dims):
     '''
     [description]
-    입력받은 args 값에 따라 모델을 선택하며, 모델이 존재하지 않을 경우 ValueError를 발생시킵니다.
+    입력받은 config 값에 따라 모델을 선택하며, 모델이 존재하지 않을 경우 ValueError를 발생시킵니다.
 
     [arguments]
-    args : argparse로 입력받은 args 값으로 이를 통해 모델을 선택합니다.
+    config : argparse로 입력받은 config 값으로 이를 통해 모델을 선택합니다.
     data : data는 data_loader로 처리된 데이터를 의미합니다.
     '''
 
